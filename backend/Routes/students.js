0efbfd315081029f62feea131749c990323d5c95
GOPIKRISHNA S <gk120000ks@gmail.com>
2023-07-22 17:08:02 +0530

    Errors

--- backend/Routes/students.js
+++ backend/Routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Student = require('../models/student');
+const Student = require('../Models/student');
 
 // Add a new student
 router.post('/students', async (req, res) => {
