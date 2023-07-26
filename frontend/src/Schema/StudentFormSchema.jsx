fb4fbac0c2d23d9367deb6f25e3c61e613ec0d2e
GOPIKRISHNA S <gk120000ks@gmail.com>
2023-07-26 15:01:02 +0530

    Just Updated (small)

--- frontend/src/Schema/StudentFormSchema.jsx
+++ frontend/src/Schema/StudentFormSchema.jsx
@@ -1,9 +1,14 @@
 import * as Yup from "yup";
 
 const StudentFormSchema = Yup.object().shape({
-  student_name: Yup.string().required("Student name is required"),
+  student_name: Yup.string().min(2).max(25).required("Please enter name"),
   email_id: Yup.string().email("Invalid email").required("Email is required"),
-  phone: Yup.string().required("Phone number is required"),
+  phone: Yup.string()
+    .required("Phone number is required")
+    .matches(
+      /^(\d{3}[-. ]?\d{3}[-. ]?\d{4}|\d{10})$/,
+      "Invalid Phonenumber format"
+    ),
   course: Yup.string().required("Course is required"),
   batch: Yup.string().required("Batch is required"),
   project: Yup.string().required("Project is required"),
