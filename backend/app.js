ae7d6a1985bc17dcfd7ac3f73718b29f59e88d7c
Uma K J <umakj1995@gmail.com>
2025-07-22 11:09:03 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 // Requiring all routes
 const userApi = require("./Routes/users");
